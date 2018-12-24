67415e4d52d8ffdb7f7ea40285fdd7268369ab7a
Your Name <you@example.com>
2018-12-24 13:59:11 +0530

    changes in response

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import store from "../js/store/index";
+import { addArticle } from "../js/actions/index";
+window.store = store;
+window.addArticle = addArticle;
+store.subscribe(() => console.log('Look ma, Redux!!'))
+store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1 }) )
